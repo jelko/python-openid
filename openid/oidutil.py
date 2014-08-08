9b2148af4be645b4d8b70c74527940c260d04c32
J. Arnds <j.arnds@dienetzgestalter.de>
2014-08-08 18:55:52 +0200

    removed lxml from elementtree_modules because of problems with lxml parsing

--- openid/oidutil.py
+++ openid/oidutil.py
@@ -14,7 +14,7 @@ import urlparse
 from urllib import urlencode
 
 elementtree_modules = [
-    'lxml.etree',
+    #'lxml.etree',
     'xml.etree.cElementTree',
     'xml.etree.ElementTree',
     'cElementTree',
